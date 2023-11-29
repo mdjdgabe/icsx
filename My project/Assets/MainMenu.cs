b280eb05699b753ee8ce92a2010dc44f51dd4b78
mdjdgabe <115570003+mdjdgabe@users.noreply.github.com>
2023-11-29 18:14:24 +0000

    Randomized game 2 and added back button

--- My project/Assets/MainMenu.cs
+++ My project/Assets/MainMenu.cs
@@ -7,6 +7,8 @@ public class MainMenu : MonoBehaviour
 {
 public string scene1Name = "island1"; // Replace with the name of your first scene
 public string scene2Name = "island1_1"; // Replace with the name of your second scene
+public string scene3Name = "island2memorygame";
+public string scene4Name = "island2slidingpuzzle";
 
 
 
@@ -23,7 +25,14 @@ public string scene2Name = "island1_1"; // Replace with the name of your second 
     public void Island2()
     {
         // Change the scene when the asset is clicked
-        SceneManager.LoadScene("island2memorygame");
+        //SceneManager.LoadScene("island2memorygame");
+
+        // Generate a random number (0 or 1) to select between scene1 and scene2
+        int randomIndex = Random.Range(0, 2);
+
+        // Load the selected scene
+        string sceneToLoad = (randomIndex == 0) ? scene3Name : scene4Name;
+        SceneManager.LoadScene(sceneToLoad);
     }
 
     public void Island3()
